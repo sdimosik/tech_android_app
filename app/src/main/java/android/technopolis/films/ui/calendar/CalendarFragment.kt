3e7a09386a5391f6c678d7899905eb0e54247979
vadimershov <vadim.ershov.01@mail.ru>
2021-04-06 18:02:29 +0300

    add library

--- app/src/main/java/android/technopolis/films/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/android/technopolis/films/ui/calendar/CalendarFragment.kt
@@ -10,16 +10,12 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import com.michalsvec.singlerowcalendar.calendar.CalendarChangesObserver
 import com.michalsvec.singlerowcalendar.calendar.CalendarViewManager
 import com.michalsvec.singlerowcalendar.calendar.SingleRowCalendar
 import com.michalsvec.singlerowcalendar.calendar.SingleRowCalendarAdapter
 import com.michalsvec.singlerowcalendar.selection.CalendarSelectionManager
 import com.michalsvec.singlerowcalendar.utils.DateUtils
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import java.util.*
 
 class CalendarFragment : Fragment(R.layout.fragment_calendar) {
@@ -41,92 +37,6 @@ class CalendarFragment : Fragment(R.layout.fragment_calendar) {
         _binding = FragmentCalendarBinding.inflate(
             inflater, container, false
         )
-
-        calendarViewModel =
-            ViewModelProvider(this).get(CalendarViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_calendar, container, false)
-
-
-        // set current date to calendar and current month to currentMonth variable
-        calendar.time = Date()
-        currentMonth = calendar[Calendar.MONTH]
-
-        // calendar view manager is responsible for our displaying logic
-        val myCalendarViewManager = object : CalendarViewManager {
-
-            override fun setCalendarViewResourceId(
-                position: Int,
-                date: Date,
-                isSelected: Boolean
-            ): Int {
-                // set date to calendar according to position where we are
-                val cal = Calendar.getInstance()
-                cal.time = date
-                // if item is selected we return this layout items
-                // in this example. monday, wednesday and friday will have special item views and other days
-                // will be using basic item view
-                return if (isSelected)
-                    R.layout.first_special_selected_calendar_item
-                else
-                // here we return items which are not selected
-                    R.layout.first_special_calendar_item
-
-                // NOTE: if we don't want to do it this way, we can simply change color of background
-                // in bindDataToCalendarView method
-            }
-
-            override fun bindDataToCalendarView(
-                holder: SingleRowCalendarAdapter.CalendarViewHolder,
-                date: Date,
-                position: Int,
-                isSelected: Boolean
-            ) {
-                // using this method we can bind data to calendar view
-                // good practice is if all views in layout have same IDs in all item views
-                holder.itemView.findViewById<TextView>(R.id.tv_date_calendar_item).text = DateUtils.getDayNumber(date)
-                holder.itemView.findViewById<TextView>(R.id.tv_day_calendar_item).text = DateUtils.getDay3LettersName(date)
-            }
-        }
-
-        // using calendar changes observer we can track changes in calendar
-        val myCalendarChangesObserver = object : CalendarChangesObserver {
-            // you can override more methods, in this example we need only this one
-            override fun whenSelectionChanged(isSelected: Boolean, position: Int, date: Date) {
-                root.findViewById<TextView>(R.id.tvDate).text = "${DateUtils.getMonthName(date)}, ${DateUtils.getDayNumber(date)} "
-                root.findViewById<TextView>(R.id.tvDay).text = DateUtils.getDayName(date)
-                super.whenSelectionChanged(isSelected, position, date)
-            }
-        }
-
-        // selection manager is responsible for managing selection
-        val mySelectionManager = object : CalendarSelectionManager {
-            override fun canBeItemSelected(position: Int, date: Date): Boolean {
-                // set date to calendar according to position
-                val cal = Calendar.getInstance()
-                cal.time = date
-                // in this example sunday and saturday can't be selected, others can
-                return true
-            }
-        }
-
-        // here we init our calendar, also you can set more properties if you haven't specified in XML layout
-        val singleRowCalendar = root.findViewById<SingleRowCalendar>(R.id.main_single_row_calendar).apply {
-            calendarViewManager = myCalendarViewManager
-            calendarChangesObserver = myCalendarChangesObserver
-            calendarSelectionManager = mySelectionManager
-            setDates(getFutureDatesOfCurrentMonth())
-            init()
-        }
-
-        root.findViewById<Button>(R.id.btnRight).setOnClickListener {
-            singleRowCalendar.setDates(getDatesOfNextMonth())
-        }
-
-        root.findViewById<Button>(R.id.btnLeft).setOnClickListener {
-            singleRowCalendar.setDates(getDatesOfPreviousMonth())
-            singleRowCalendar.calendarSelectionManager = mySelectionManager
-        }
-
 
         return binding.root
     }
@@ -177,11 +87,90 @@ class CalendarFragment : Fragment(R.layout.fragment_calendar) {
 
         calendarViewModel = (activity as MainActivity).calendarViewModel
 
-        calendarViewModel.text
-            .onEach { text ->
-                binding.textFeed.text = text
+        // using calendar changes observer we can track changes in calendar
+        val myCalendarChangesObserver = object : CalendarChangesObserver {
+            // you can override more methods, in this example we need only this one
+            override fun whenSelectionChanged(isSelected: Boolean, position: Int, date: Date) {
+                binding.root.findViewById<TextView>(R.id.tvDate).text = "${DateUtils.getMonthName(date)}, ${DateUtils.getDayNumber(date)} "
+                binding.root.findViewById<TextView>(R.id.tvDay).text = DateUtils.getDayName(date)
+                super.whenSelectionChanged(isSelected, position, date)
             }
-            .launchIn(lifecycleScope)
+        }
+
+        // calendar view manager is responsible for our displaying logic
+        val myCalendarViewManager = object : CalendarViewManager {
+
+            override fun setCalendarViewResourceId(
+                position: Int,
+                date: Date,
+                isSelected: Boolean
+            ): Int {
+                // set date to calendar according to position where we are
+                val cal = Calendar.getInstance()
+                cal.time = date
+                // if item is selected we return this layout items
+                // in this example. monday, wednesday and friday will have special item views and other days
+                // will be using basic item view
+                return if (isSelected)
+                    R.layout.first_special_selected_calendar_item
+                else
+                // here we return items which are not selected
+                    R.layout.first_special_calendar_item
+
+                // NOTE: if we don't want to do it this way, we can simply change color of background
+                // in bindDataToCalendarView method
+            }
+
+            override fun bindDataToCalendarView(
+                holder: SingleRowCalendarAdapter.CalendarViewHolder,
+                date: Date,
+                position: Int,
+                isSelected: Boolean
+            ) {
+                // using this method we can bind data to calendar view
+                // good practice is if all views in layout have same IDs in all item views
+                holder.itemView.findViewById<TextView>(R.id.tv_date_calendar_item).text = DateUtils.getDayNumber(date)
+                holder.itemView.findViewById<TextView>(R.id.tv_day_calendar_item).text = DateUtils.getDay3LettersName(date)
+            }
+        }
+
+        // selection manager is responsible for managing selection
+        val mySelectionManager = object : CalendarSelectionManager {
+            override fun canBeItemSelected(position: Int, date: Date): Boolean {
+                // set date to calendar according to position
+                val cal = Calendar.getInstance()
+                cal.time = date
+                // in this example sunday and saturday can't be selected, others can
+                return true
+            }
+        }
+
+        val singleRowCalendar = view.findViewById<SingleRowCalendar>(R.id.main_single_row_calendar).apply {
+            calendarViewManager = myCalendarViewManager
+            calendarChangesObserver = myCalendarChangesObserver
+            calendarSelectionManager = mySelectionManager
+            setDates(getFutureDatesOfCurrentMonth())
+            init()
+        }
+
+        view.findViewById<Button>(R.id.btnRight).setOnClickListener {
+            singleRowCalendar.setDates(getDatesOfNextMonth())
+        }
+
+        view.findViewById<Button>(R.id.btnLeft).setOnClickListener {
+            singleRowCalendar.setDates(getDatesOfPreviousMonth())
+            singleRowCalendar.calendarSelectionManager = mySelectionManager
+        }
+        // set current date to calendar and current month to currentMonth variable
+        calendar.time = Date()
+        currentMonth = calendar[Calendar.MONTH]
+
+
+//        calendarViewModel.text
+//            .onEach { text ->
+//                binding.textCalendar.text = text
+//            }
+//            .launchIn(lifecycleScope)
     }
 
     override fun onDestroy() {
