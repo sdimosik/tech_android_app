f7b84d1c2237498636cf4feea0d3326c68934984
IgorSamohin <igor04012001@yandex.ru>
2021-05-03 12:26:53 +0300

    Use bindings and appCompat views in media

--- app/src/main/java/android/technopolis/films/ui/watch/rvMediaHolder/MediaAdapter.kt
+++ app/src/main/java/android/technopolis/films/ui/watch/rvMediaHolder/MediaAdapter.kt
@@ -1,20 +1,18 @@
 package android.technopolis.films.ui.watch.rvMediaHolder
 
-import android.technopolis.films.R
+import android.technopolis.films.databinding.MediaHolderBinding
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
 class MediaAdapter : ListAdapter<Media, MediaViewHolder>(MediaDiff) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MediaViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.media_holder, parent, false)
-        return MediaViewHolder(view)
+        val binding = MediaHolderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MediaViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MediaViewHolder, position: Int) {
@@ -22,11 +20,12 @@ class MediaAdapter : ListAdapter<Media, MediaViewHolder>(MediaDiff) {
     }
 }
 
-class MediaViewHolder(itemView: View) : ViewHolder(itemView) {
-    private val ivImage: ImageView = itemView.findViewById(R.id.media_holder__media_image)
-    private val tvName: TextView = itemView.findViewById(R.id.media_holder__media_name)
-    private val tvDescription: TextView =
-        itemView.findViewById(R.id.media_holder__media_description)
+class MediaViewHolder(
+    binding: MediaHolderBinding
+) : ViewHolder(binding.root) {
+    private val ivImage: AppCompatImageView = binding.mediaHolderMediaImage
+    private val tvName: AppCompatTextView = binding.mediaHolderMediaName
+    private val tvDescription: AppCompatTextView = binding.mediaHolderMediaDescription
 
     fun bind(media: Media) {
         ivImage.setBackgroundColor(0x00FF00)
