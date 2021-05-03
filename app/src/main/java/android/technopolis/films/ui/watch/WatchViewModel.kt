c8834b767748557a575ba25af1e56d8d140289fd
IgorSamohin <igor04012001@yandex.ru>
2021-05-03 12:28:25 +0300

    Hide public field

--- app/src/main/java/android/technopolis/films/ui/watch/WatchViewModel.kt
+++ app/src/main/java/android/technopolis/films/ui/watch/WatchViewModel.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.technopolis.films.repository.MainRepository
 import android.technopolis.films.repository.Repository
 import android.technopolis.films.ui.watch.rvMediaHolder.Media
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -17,10 +19,9 @@ class WatchViewModel : ViewModel() {
     private val _text = MutableStateFlow("This is watch Fragment")
     val text: StateFlow<String> = _text.asStateFlow()
 
-    private val _repository: Repository = MainRepository()
-    val repository: Repository get() = _repository
+    private val repository: Repository = MainRepository()
 
-    val medias: MutableLiveData<List<Media>> = MutableLiveData(
+    private val _medias: MutableLiveData<List<Media>> = MutableLiveData(
         listOf(
             Media(1),
             Media(1),
@@ -31,4 +32,13 @@ class WatchViewModel : ViewModel() {
             Media(1),
         )
     )
+    val medias: LiveData<List<Media>> = _medias
+
+    fun onPageChanged(savedState: Bundle) {
+        tabArgs.putAll(savedState)
+    }
+
+    fun getTabProperty(name: String): Any? {
+        return tabArgs.get(name)
+    }
 }
